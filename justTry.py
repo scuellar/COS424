096e26102761e95b0f21c1096c9ba477162e0c45
Jonathan Balkind <jbalkind@cs.princeton.edu>
2014-04-16 16:51:06 -0400

    business, not bussiness

--- justTry.py
+++ justTry.py
@@ -4,16 +4,14 @@ import businessVectors
 
 def getWords(i=0):
     (l, d) = readYelp.readY('../yelp/yelp_academic_dataset_review.json', i)
-    #(l, d) = readYelp.readY('../../hw1/yelp/yelp_academic_dataset_review.json', 50000)
     print "Got the data! Got ", l, " lines of data"
     (all_w, per) = words.bagOfWords(d)
     return (all_w, per)
 
-def getBussVector(i=0):
+def getBusVector(i=0):
     (l, d) = readYelp.readY('../yelp/yelp_academic_dataset_business.json', i)
-    #(l, d) = readYelp.readY('../../hw1/yelp/yelp_academic_dataset_review.json', 50000)
     print "Got the data! Got ", l, " lines of data"
-    (n, vectors) = businessVectors.bussFeatureVector(d)
+    (n, vectors) = businessVectors.busFeatureVector(d)
     return (n, vectors)
 
 
