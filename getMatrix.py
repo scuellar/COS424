78e909e97c2b809275bb42b74eb580b995720524
Santiago <scuellar@princeton.edu>
2014-05-02 15:28:12 -0400

    some changes

--- getMatrix.py
+++ getMatrix.py
@@ -1,37 +1,41 @@
 import justTry
+
 from numpy import array
 from scipy.sparse import lil_matrix
 from scipy.io import mmwrite
 
-def getMatrix(filename):
-    
-	reviews = justTry.getUserReviews()
+def getMatrix(filename, n=0):
+
+    reviews = justTry.getUserReviews(n)
 	
-	user_IDs = reviews.keys()
-	nUsers = len(user_IDs)
+    user_IDs = reviews.keys()
+    nUsers = len(user_IDs)
 	
-	bus = justTry.getBusVector()
-	bus = bus[1]
-	bus_IDs = bus.keys()
-	nBus = len(bus_IDs)
+    bus = justTry.getBusVector(n)
+    bus = bus[1]
+    bus_IDs = bus.keys()
+    nBus = len(bus_IDs)
 	# turn bus' value into column ids
-	for i, bid in enumerate(bus_IDs):
-		bus[bid] = i
+    for i, bid in enumerate(bus_IDs):
+        bus[bid] = i
 
 	# full array is very large, outputting as Matrix Market format later on
-	rmatrix = lil_matrix((nUsers,nBus))
+    rmatrix = lil_matrix((nUsers,nBus))
 
 	# for each userID (row), find all ratings and place them in the appropriate location in rmatrix
-	for row, uid in enumerate(user_IDs):
-		revs = reviews[uid]						# reviews this user made (dic)
-		rev_IDs = reviews[uid].keys()			# ids of businesses this user has reviewed
-		for bid in rev_IDs:	
-			if bid in bus:						# some businesses not in our list				
-				col = bus[bid] 						# column associated with business
-				rmatrix[row,col] = revs[bid]
+    for row, uid in enumerate(user_IDs):
+        revs = reviews[uid]						# reviews this user made (dic)
+        rev_IDs = reviews[uid].keys()			# ids of businesses this user has reviewed
+        for bid in rev_IDs:	
+            if bid in bus:						# some businesses not in our list				
+                col = bus[bid] 						# column associated with business
+                rmatrix[row,col] = revs[bid]
+                
+    print "almost done"
+    mmwrite(filename, rmatrix) 		# 70817 x 3654
+    return rmatrix
 
-	mmwrite(filename, rmatrix) 		# 70817 x 3654
 
 
-getMatrix("ratings_matrix.mtx")
+#mmwrite("fact_ratings_matrix.mtx", fctr_res)
 
